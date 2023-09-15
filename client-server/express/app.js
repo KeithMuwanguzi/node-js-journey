c07f617b47e2360f88402de4e9b1b3567d41c261
KeithAnnonMM <keithmuwanguzi@gmail.com>
2023-09-15 13:04:15 +0300

    Middleware

--- client-server/express/app.js
+++ client-server/express/app.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.set('view engine','ejs')
+
+//middleware
+app.use(express.static('public'))
+
+app.use(morgan('dev'))
 
 app.get('/',(req,res)=>{
     const blogs = [
